8512091fbc2d9d9176f93a7532779c5b607b1d0b
noddysnots <pantsarthak6@gmail.com>
2025-05-23 10:33:01 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: "export",
+  basePath: "/surveyor-kpi",
+  assetPrefix: "/surveyor-kpi"
 };
 
 export default nextConfig;
